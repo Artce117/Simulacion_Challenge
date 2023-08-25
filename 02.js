db9d690ff51538f534a04c1776da858733dae318
Dandrescepeda <artce117@gmail.com>
2023-08-25 16:06:36 -0500

    avance simulacion

--- 02.js
+++ 02.js
@@ -12,6 +12,13 @@ function stringMasLarga(strings) {
   // stringMasLarga(['JavaScript', 'HTML', 'CSS']); debe retornar 'JavaScript'
 
   // Tu código aca
+  let newArray = strings[0];
+  for (let i = 1; i < strings.length; i++) {
+    if (strings[i].length >= newArray.length) {
+      newArray = strings[i];
+    }
+  }
+  return newArray;
 }
 
 // No modifiques nada debajo de esta linea //
